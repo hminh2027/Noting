c52c64bf1539deddb90542b5288ad1f3ec1a231f
ddanh712 <doanduyanh0712@gmail.com>
2022-11-01 18:24:12 +0700

    login input gr

--- web/pages/test.js
+++ web/pages/test.js
@@ -1,30 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { NoteMainTemplate } from "../src/components/UI/template/Note/";
+import ButtonLogin from "../src/components/UI/atoms/LoginForm/ButtonLogin";
+import LoginInputGroup from "../src/components/UI/molecules/LoginInput/LoginInputGroup";
 
-import LoginForm from "../src/components/UI/template/LoginForm";
 
-import noteApi from "../src/api/apis/noteApi";
-import NoteTaking from "../src/components/UI/template/NoteTaking";
-import dynamic from "next/dynamic";
-import Input from "../src/components/UI/atoms/LoginInput";
-const NoteEditor = dynamic(() => import("../src/components/Editor"), {
-  ssr: false,
-});
+
 const Test = () => {
-  console.log(process.env.EXAMPLE_API_URL);
-  const [apiResult, setApiResult] = useState([]);
-  useEffect(() => {
-    const fetchApi = async () => {
-      const res = await noteApi.get();
-      setApiResult(res);
-      console.log(res);
-    };
 
-    fetchApi();
-  }, []);
 
   return (
-    <Input/>
+    <div>
+       <LoginInputGroup/>
+      <ButtonLogin/>
+    </div>
+   
   );
 };
 
---
+++ web/src/components/UI/atoms/LoginForm/ButtonLogin.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, ButtonGroup, WrapItem } from '@chakra-ui/react'
+const ButtonLogin = () => {
+
+  return (
+   <Button
+   colorScheme='orange' variant='solid'
+  width= '100%'
+  borderRadius='20px'
+  borderColor='none'
+>
+   Login
+</Button>
+  );
+};
+
+export default ButtonLogin;
