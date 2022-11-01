5d717905d40e9af63fb11448c3429b16aebeb15e
ddanh712 <doanduyanh0712@gmail.com>
2022-11-01 20:53:55 +0700

    edit test.js

--- web/pages/test.js
+++ web/pages/test.js
@@ -3,8 +3,10 @@ import AvatarUserLogin from "../src/components/UI/atoms/LoginForm/AvatarUserLogi
 import ButtonLogin from "../src/components/UI/atoms/LoginForm/ButtonLogin";
 import LoginInputGroup from "../src/components/UI/molecules/LoginInput/LoginInputGroup";
 
-const Test = ({ noteCategories, notes }) => {
-  return <NoteMainTemplate categories={noteCategories} notes={notes} />;
+const Test = () => {
+  return (
+    <AvatarUserLogin></AvatarUserLogin>
+  )
 };
 
 export default Test;
