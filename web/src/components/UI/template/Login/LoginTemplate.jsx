1d754e514411dfc3f8493fea40b3325f0c0dec66
ddanh712 <doanduyanh0712@gmail.com>
2022-11-02 20:40:33 +0700

    login form

--- web/src/components/UI/template/Login/LoginTemplate.jsx
+++ web/src/components/UI/template/Login/LoginTemplate.jsx
@@ -6,14 +6,10 @@ import LoginInputGroup from "../../molecules/LoginInput/LoginInputGroup";
 
 const LoginTemplate = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full p-6">
-      <div>
-        <img className="pl-20 pt-10 h-4/5 object-cover" src="https://cdn2.iconfinder.com/data/icons/work-from-home-malibu-vol-1/128/Note_Taking_App-512.png" alt="" />
-      </div>
-
-      <div className="flex flex-col justify-center pb-20 pr-20 mr-20">
-         <form className="max-w-[500px] w-full mx-auto px-8 ">
-            <AvatarUserLogin></AvatarUserLogin>
+    <div className=" flex justify-center">
+      <div className=" flex flex-col justify-center border border-gray-300 w-[500px] ">
+         <form className="max-w-[500px] w-full mx-auto pt-20 p-10">
+            <AvatarUserLogin className="mb-10"></AvatarUserLogin>
             <LoginInputGroup 
               className="pt-6"
               />
