39ce02b6c427a091cdfbda19a2f8ac5a8fd2bbf7
pta208VDT <pta20801@gmail.com>
2022-11-05 10:14:59 +0700

    Delete unsed import

--- web/pages/login.js
+++ web/pages/login.js
@@ -1,18 +1,8 @@
 import React, { useEffect, useState } from "react";
-import AvatarUserLogin from "../src/components/UI/atoms/LoginForm/AvatarUserLogin";
-import ButtonLogin from "../src/components/UI/atoms/LoginForm/ButtonLogin";
-import LoginInputGroup from "../src/components/UI/molecules/LoginInput/LoginInputGroup";
 import LoginTemplate from "../src/components/UI/template/Login/LoginTemplate";
 
-
-
-const Test = () => {
-
-
-  return (
-    <LoginTemplate></LoginTemplate>
-   
-  );
+const Login = () => {
+  return <LoginTemplate></LoginTemplate>;
 };
 
 export default Test;
