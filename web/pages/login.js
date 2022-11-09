e4cde602aeed31c1fbaa691724d2693ea49109a9
ddanh712 <doanduyanh0712@gmail.com>
2022-11-09 16:28:42 +0700

    searchinput modal

--- web/pages/login.js
+++ web/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AvatarUserLogin from "../src/components/UI/atoms/LoginForm/AvatarUserLogin";
 import ButtonLogin from "../src/components/UI/atoms/LoginForm/ButtonLogin";
+import SearchInput from "../src/components/UI/atoms/Modal/SearchInput";
 import LoginInputGroup from "../src/components/UI/molecules/LoginInput/LoginInputGroup";
 import LoginTemplate from "../src/components/UI/template/Login/LoginTemplate";
 
@@ -8,8 +9,10 @@ import LoginTemplate from "../src/components/UI/template/Login/LoginTemplate";
 
 const Login = () => {
   return (
+ 
     <LoginTemplate/>
-   
+  
+
   );
 };
 
