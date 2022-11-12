9fd05bcab16b442735742ecfd80c183e3adb8bd4
hminh2027 <dackheros@gmail.com>
2022-11-12 20:07:22 +0700

    merge shared controller into note controller

--- api/src/modules/shared-note/shared-note.module.ts
+++ api/src/modules/shared-note/shared-note.module.ts
@@ -2,11 +2,9 @@ import { NoteModule } from './../note/note.module';
 import { SharedNote } from './shared-note.entity';
 import { Module } from '@nestjs/common';
 import { SharedNoteService } from './shared-note.service';
-import { SharedNoteController } from './shared-note.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [SharedNoteController],
   imports: [TypeOrmModule.forFeature([SharedNote])],
   exports: [SharedNoteService],
   providers: [SharedNoteService],
